f919115991f8add15c4f27291ef8a503f3e5f3ab
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-12-24 21:45:54 +0530

    update-skills
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- src/data/businessSkills.js
+++ src/data/businessSkills.js
@@ -1,53 +1,54 @@
-const experience = ["Begineer", "Intermediary", "Expert"]
-const experienceColor = ["#1F8D00", "#FFE000", "#FF7200"]
+const experience = ["Begineer", "Intermediary", "Expert"];
+const experienceColor = ["#1F8D00", "#FFE000", "#FF7200"];
 
 export default [
-	{
-		id: 1,
-		title: "Slack",
-		icon: "https://img.stackshare.io/service/675/RNiSRYOF_400x400.jpg",
-		experience: experience[2],
-		experienceColor: experienceColor[2],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-	{
-		id: 2,
-		title: "Jira",
-		icon: "https://img.stackshare.io/service/154/Qifq4jpS_400x400.jpg",
-		experience: experience[2],
-		experienceColor: experienceColor[2],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-	{
-		id: 3,
-		title: "Trello",
-		icon: "https://img.stackshare.io/service/109/-CvHThPk_400x400.jpg",
-		experience: experience[2],
-		experienceColor: experienceColor[2],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-	{
-		id: 4,
-		title: "Confluence",
-		icon: "https://img.stackshare.io/service/769/5_z16TbH_400x400.jpg",
-		experience: experience[1],
-		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-	{
-		id: 5,
-		title: "Asana",
-		icon: "https://img.stackshare.io/service/108/asana.png",
-		experience: experience[2],
-		experienceColor: experienceColor[2],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-	{
-		id: 6,
-		title: "Monday",
-		icon: "https://img.stackshare.io/service/9761/monday_logo_icon.png",
-		experience: experience[1],
-		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
-	},
-]
+  {
+    id: 1,
+    title: "Slack",
+    icon: "https://img.stackshare.io/service/675/RNiSRYOF_400x400.jpg",
+    experience: experience[2],
+    experienceColor: experienceColor[2],
+    url: "https://github.com/bhimeshchauhan",
+  },
+  {
+    id: 2,
+    title: "Jira",
+    icon: "https://img.stackshare.io/service/154/Qifq4jpS_400x400.jpg",
+    experience: experience[2],
+    experienceColor: experienceColor[2],
+    url: "https://github.com/bhimeshchauhan",
+  },
+  {
+    id: 3,
+    title: "Trello",
+    icon: "https://img.stackshare.io/service/109/-CvHThPk_400x400.jpg",
+    experience: experience[2],
+    experienceColor: experienceColor[2],
+    url: "https://github.com/bhimeshchauhan",
+  },
+  {
+    id: 4,
+    title: "Confluence",
+    icon: "https://img.stackshare.io/service/769/5_z16TbH_400x400.jpg",
+    experience: experience[1],
+    experienceColor: experienceColor[1],
+    url: "https://github.com/bhimeshchauhan",
+  },
+  {
+    id: 5,
+    title: "Asana",
+    icon:
+      "https://img.stackshare.io/service/108/default_d91a59e7b4dde497605e10e1cf80eb445904a438.jpg",
+    experience: experience[2],
+    experienceColor: experienceColor[2],
+    url: "https://github.com/bhimeshchauhan",
+  },
+  {
+    id: 6,
+    title: "Monday",
+    icon: "https://img.stackshare.io/service/9761/monday_logo_icon.png",
+    experience: experience[1],
+    experienceColor: experienceColor[1],
+    url: "https://github.com/bhimeshchauhan",
+  },
+];
--- src/data/utilskills.js
+++ src/data/utilskills.js
@@ -8,15 +8,15 @@ export default [
 		icon: "https://img.stackshare.io/service/1336/xWMRvm_5_400x400.png",
 		experience: experience[1],
 		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 	{
 		id: 2,
 		title: "Github Pages",
-		icon: "https://img.stackshare.io/service/683/sBsvBbjY.png",
+		icon: "https://img.stackshare.io/service/683/default_5eeb660db1239eeb3f1c1f6187f2abf0f3a0500d.png",
 		experience: experience[2],
 		experienceColor: experienceColor[2],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 	{
 		id: 3,
@@ -24,7 +24,7 @@ export default [
 		icon: "https://img.stackshare.io/service/97/eW6tXeq3.png",
 		experience: experience[1],
 		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 	{
 		id: 4,
@@ -32,7 +32,7 @@ export default [
 		icon: "https://img.stackshare.io/service/130/VrwpNfjh_400x400.png",
 		experience: experience[1],
 		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 	{
 		id: 5,
@@ -40,7 +40,7 @@ export default [
 		icon: "https://img.stackshare.io/service/4507/oauth2-logo.png",
 		experience: experience[1],
 		experienceColor: experienceColor[1],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 	{
 		id: 6,
@@ -48,6 +48,6 @@ export default [
 		icon: "https://img.stackshare.io/service/2038/PSpUS-Hp_400x400.jpg",
 		experience: experience[0],
 		experienceColor: experienceColor[0],
-		url: "https://github.com/bhimeshchauhan/connect_four",
+		url: "https://github.com/bhimeshchauhan",
 	},
 ]
