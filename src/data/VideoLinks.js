8d249f4ff627ef031399e97dfffc995ac54a29ac
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-12-25 18:46:10 +0530

    change-order
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- src/data/VideoLinks.js
+++ src/data/VideoLinks.js
@@ -3,17 +3,6 @@ import video from "../assets/images/projects/video.svg";
 export default [
   {
     id: 1,
-    title: "James Webb Telescope - NASA",
-    icon: video,
-    description: "The launch of James Webb Telescope in 2021",
-    githubPath: null,
-    media: [],
-    demoPath: null,
-    iframe: `<iframe width="1190" height="669" src="https://www.youtube.com/embed/7nT7JGZMbtM?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
-    year: "2021",
-  },
-  {
-    id: 2,
     title: "ISS",
     icon: video,
     description: "ISS",
@@ -21,6 +10,17 @@ export default [
     media: [],
     demoPath: null,
     iframe: `<iframe width="1190" height="669" src="https://www.youtube.com/embed/86YLFOog4GM?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    year: "2021",
+  },
+  {
+    id: 2,
+    title: "James Webb Telescope - NASA",
+    icon: video,
+    description: "The launch of James Webb Telescope in 2021",
+    githubPath: null,
+    media: [],
+    demoPath: null,
+    iframe: `<iframe width="1190" height="669" src="https://www.youtube.com/embed/7nT7JGZMbtM?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
     year: "2021",
   },
 ];
