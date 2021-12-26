2cc41b71b839db5e810723481378e3f56242e19a
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-12-26 10:24:52 +0530

    add-helmet
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,11 +1,10 @@
 const { hot } = require("react-hot-loader/root")
 
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/bhimeshchauhan/Desktop/bhimeshchauhan.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/bhimeshchauhan/Desktop/bhimeshchauhan.github.io/src/pages/404.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/bhimeshchauhan/Desktop/bhimeshchauhan.github.io/src/pages/blog.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/bhimeshchauhan/Desktop/bhimeshchauhan.github.io/src/pages/contact.js"))),
