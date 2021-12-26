2e94a94355e3f8c2e5856e856977720593a95df9
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-12-26 10:15:42 +0530

    add-analytics
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import BhimeshIcon from '../assets/images/coding.gif'
-import AboutBox from '../components/About/AboutBox'
-import about from '../data/About'
-import { UserWrapper, UserTitle, UserDescription, DownloadButton, UserTopic } from '../styles/bhimeshStyle.js'
-import '../styles/bhimeshStyle.css'
-import Layout from '../components/layout'
+import React from "react";
+import BhimeshIcon from "../assets/images/coding.gif";
+import AboutBox from "../components/About/AboutBox";
+import about from "../data/About";
+import {
+  UserWrapper,
+  UserTitle,
+  UserDescription,
+  DownloadButton,
+  UserTopic,
+} from "../styles/bhimeshStyle.js";
+import "../styles/bhimeshStyle.css";
+import Layout from "../components/layout";
 
 const IndexPage = () => {
-  return <Layout>
-  <UserWrapper>
-    <UserTitle>Bhimesh <span>Chauhan</span></UserTitle>
-    <UserDescription>
-      <div>
-        <p>
-        Hello there, thank you for checking out my website. I am a
-        Full-Stack Engineer with specialization in Computer Vision and Graphic utilizing ML.
-        </p>
-        <DownloadButton href="https://pdfhost.io/v/WSqSdrSWI_Microsoft_Word_Resumedoc.pdf"  download title="Resume">Download Resume</DownloadButton>
-      </div>
-      <img src={BhimeshIcon} alt="bhimesh self" />
-    </UserDescription>
-    <UserTopic>
-      {
-        about.map(item => (<AboutBox key={item.id} info={item} />))
-      }
-    </UserTopic>
-  </UserWrapper>
-  </Layout>
-}
+  return (
+    <Layout>
+      <UserWrapper>
+        <UserTitle>
+          Bhimesh <span>Chauhan</span>
+        </UserTitle>
+        <UserDescription>
+          <div>
+            <p>
+              Hello there, thank you for checking out my website. I am a
+              Full-Stack Engineer with specialization in Computer Vision and
+              Graphic utilizing ML.
+            </p>
+            <DownloadButton
+              href="https://pdfhost.io/v/WSqSdrSWI_Microsoft_Word_Resumedoc.pdf"
+              download
+              title="Resume"
+            >
+              Download Resume
+            </DownloadButton>
+          </div>
+          <img src={BhimeshIcon} alt="bhimesh self" />
+        </UserDescription>
+        <UserTopic>
+          {about.map((item) => (
+            <AboutBox key={item.id} info={item} />
+          ))}
+        </UserTopic>
+      </UserWrapper>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
