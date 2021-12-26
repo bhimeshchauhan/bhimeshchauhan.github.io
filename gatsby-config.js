d8f29565acb4d2b906042820c6c9b826040b7fec
bhimeshchauhan <bhimeshchauhan@gmail.com>
2021-12-26 10:15:42 +0530

    add-analytics
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,60 @@
 module.exports = {
   siteMetadata: {
-    title: 'Bhimesh Chauhan',
-    siteUrl: 'http://bhimeshchauhan.github.io',
+    title: "Bhimesh Chauhan",
+    siteUrl: "http://bhimeshchauhan.github.io",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           `Signika`,
-          `source sans pro\:300,400,600` // you can also specify font weights and styles
-        ]
-      }
+          `source sans pro\:300,400,600`, // you can also specify font weights and styles
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-130172537-1",
         head: false,
-      }
+      },
     },
     {
-      resolve: `gatsby-transformer-sharp`
-    }
+      resolve: `gatsby-transformer-sharp`,
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-MXN2LDQ2MB",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "github.io",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
-}
+};
