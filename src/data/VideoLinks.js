10c8e84786f6cb7402408bb829a46f1ba7945999
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-01-19 12:33:08 +0530

    edit-link
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- src/data/VideoLinks.js
+++ src/data/VideoLinks.js
@@ -9,7 +9,7 @@ export default [
     githubPath: null,
     media: [],
     demoPath: null,
-    iframe: `<iframe width="1664" height="769" src="https://www.youtube.com/embed/r8NhyRjBxnE?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    iframe: `<iframe width="1664" height="769" src="https://www.youtube.com/embed/4E5q-TvNh1I?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
     year: "2021",
   },
   {
