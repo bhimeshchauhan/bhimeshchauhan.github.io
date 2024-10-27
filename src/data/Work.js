cc673f5129b4c549186d8bb14bc5beae37dc0c94
Bhimesh Chauhan <bhimeshchauhan@gmail.com>
2024-10-26 21:42:08 -0400

    reduce-size

--- src/data/Work.js
+++ src/data/Work.js
@@ -4,73 +4,87 @@ import sodima from "../assets/images/experience/bhimesh/sodima.png";
 import nebullam from "../assets/images/experience/bhimesh/nebullam.png";
 import solum from "../assets/images/experience/bhimesh/solum.png";
 import preply from "../assets/images/experience/bhimesh/preply.png";
+import micromart from "../assets/images/experience/bhimesh/micromart.png";
 
 export default [
 	{
 		id: 1,
+		name: "MicroMart / KitchenMate Inc.",
+		designation: "Senior Full Stack Software Engineer",
+		dated: "",
+		logo: micromart,
+		companyLink: "https://www.preply.com/",
+		description:
+			'● Developed an interactive dashboard to manage kiosks inreal-time, improving operational efficiency.<br/>' +
+			'● Built clients for Modbus RS485 protocols to interface with smart fridges, integrating low-level hardware with software using sockets and servers.<br/>' +
+			'● Led the certification and integration of Heartland payment systems, ensuring PCI compliance.<br/>' +
+			'● Collaborated with external vendors and partners to ensure successful API integrations.'
+	},
+	{
+		id: 2,
 		name: "Preply Inc.",
 		designation: "Senior Full Stack Software Engineer",
 		dated: "",
 		logo: preply,
 		companyLink: "https://www.preply.com/",
 		description:
-			'• Developed Application in React to connect users on their learning journey with tutors<br/>' +
-			'• Launch and own progressive A/B test driving product to better serve clients and increase NSS metrics for the company<br/>' +
-			'• Monitor the test data and make business decision on feature release for various markets'
+			'● Developed Application in React to connect users on their learning journey with tutors<br/>' +
+			'● Launch and own progressive A/B test driving product to better serve clients and increase NSS metrics for the company<br/>' +
+			'● Monitor the test data and make business decision on feature release for various markets'
 	},
 	{
-		id: 2,
+		id: 3,
 		name: "DataRobot Inc.",
 		designation: "Full Stack Software Engineer",
 		dated: "",
 		logo: dr,
 		companyLink: "https://www.datarobot.com/",
 		description:
-			'• Developed Application in React to connect and write to MSSQL, RedShift, and other types of servers.<br/>' +
-			'• Integrated Datarobot Tools to third party applications like Microsoft Excel, Snowflake, Tableau. Integrated testing process using Jenkins.'
+			'● Developed Application in React to connect and write to MSSQL, RedShift, and other types of servers.<br/>' +
+			'● Integrated Datarobot Tools to third party applications like Microsoft Excel, Snowflake, Tableau. Integrated testing process using Jenkins.'
 	},
 	{
-		id: 3,
+		id: 4,
 		name: "Pitchly Inc.",
 		designation: "Applications Engineer",
 		dated: "",
 		logo: pitchly,
 		companyLink: "https://www.datarobot.com/",
 		description:
-			'• Designed an MVC application with API design to respond to various intricate requests to play well in the application ecosystem.<br/>'+
-            '• Wrote two applications in Meteor to use GraphQL endpoints in an ecosystem to help financial & legal institutions streamline data.'
+			'● Designed an MVC application with API design to respond to various intricate requests to play well in the application ecosystem.<br/>'+
+            '● Wrote two applications in Meteor to use GraphQL endpoints in an ecosystem to help financial & legal institutions streamline data.'
 	},
 	{
-		id: 4,
+		id: 5,
 		name: "Nebullam Inc.",
 		designation: "Android Application Engineer and Data Scientist",
 		dated: "",
 		logo: nebullam,
 		companyLink: "https://www.datarobot.com/",
 		description:
-			'• Built an android application to remotely control and monitor the plant growth on high pressured aeroponics systems to increase the yield.<br/>'+
-            '• Implemented a neural network using TensorFlow to study and automate the generation of plant growth which helped raise investment.'
+			'● Built an android application to remotely control and monitor the plant growth on high pressured aeroponics systems to increase the yield.<br/>'+
+            '● Implemented a neural network using TensorFlow to study and automate the generation of plant growth which helped raise investment.'
 	},
 	{
-		id: 5,
+		id: 6,
 		name: "Sodima Solutions",
 		designation: "Full Stack Software Engineer",
 		dated: "",
 		logo: sodima,
 		companyLink: "https://www.datarobot.com/",
 		description:
-			'• Programmed 3 Million agricultural landmass datasets to purchase & lease in MeteorJS with D3JS on the Nginx server, deployed on Docker.<br/>'+
-            '• Developed MVP architecture for an app in GraphQL to build NLP chatbot.'
+			'● Programmed 3 Million agricultural landmass datasets to purchase & lease in MeteorJS with D3JS on the Nginx server, deployed on Docker.<br/>'+
+            '● Developed MVP architecture for an app in GraphQL to build NLP chatbot.'
 	},
 	{
-		id: 6,
+		id: 7,
 		name: "Solum Labs - Climate Corporation",
 		designation: "Full Stack Software Engineer",
 		dated: "",
 		logo: solum,
 		companyLink: "https://www.datarobot.com/",
 		description:
-            '• Redesigned the MVC model to read NFC to assist in faster and error-free data retrieval of 1500 samples per day to test nutrients in the soil.<br/>'+
-            '• Constructed an API for plasma spectrometer using WAMP, Crossbar.io, D3.js, and Leaflet for visualization of 180 samples per hour.'
+            '● Redesigned the MVC model to read NFC to assist in faster and error-free data retrieval of 1500 samples per day to test nutrients in the soil.<br/>'+
+            '● Constructed an API for plasma spectrometer using WAMP, Crossbar.io, D3.js, and Leaflet for visualization of 180 samples per hour.'
 	}
 ];
